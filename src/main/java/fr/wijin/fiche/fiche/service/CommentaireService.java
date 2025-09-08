a2843d2e0d7a8f87709d2e674e3c0718866dfa2d
HeleneHinde <helene.herve@outlook.com>
2025-09-08 14:25:52 +0200

    Refactor CommentaireService and FicheService to include authentication checks and set authors for comments and fiches

--- src/main/java/fr/wijin/fiche/fiche/service/CommentaireService.java
+++ src/main/java/fr/wijin/fiche/fiche/service/CommentaireService.java
@@ -1,27 +1,39 @@
 package fr.wijin.fiche.fiche.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import fr.wijin.fiche.fiche.dto.CommentaireDto;
 import fr.wijin.fiche.fiche.mapper.CommentaireMapper;
+import fr.wijin.fiche.fiche.mapper.UserMapper;
 import fr.wijin.fiche.fiche.model.Commentaire;
 import fr.wijin.fiche.fiche.repository.CommentaireRepository;
+import fr.wijin.fiche.fiche.repository.UserRepository;
 import jakarta.transaction.Transactional;
 
 @Transactional
 @Service
 public class CommentaireService {
-    
-        @Autowired
+
+	@Autowired
 	private CommentaireRepository commentaireRepository;
-	
+
 	@Autowired
 	private CommentaireMapper commentaireMapper;
-	
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserMapper userMapper;
+
 	public List<CommentaireDto> findAll() {
 		return commentaireRepository
 				.findAll()
@@ -32,9 +44,9 @@ public class CommentaireService {
 
 	public Optional<CommentaireDto> findById(Integer id) {
 		Commentaire commentaire = commentaireRepository.findById(id)
-					.orElse(null);
+				.orElse(null);
 		if (commentaire == null) {
-			return Optional.ofNullable( (CommentaireDto) null);
+			return Optional.ofNullable((CommentaireDto) null);
 		}
 		return Optional.of(
 				commentaireMapper.mapCommentaireToCommentaireDto(commentaire));
@@ -45,6 +57,12 @@ public class CommentaireService {
 	}
 
 	public CommentaireDto save(CommentaireDto commentaire) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			commentaire.setAuteurCommentaire(
+					userMapper.mapUserToUserDto(userRepository.findByUsername(authentication.getName())));
+		}
+		commentaire.setDate(LocalDateTime.now());
 		return commentaireMapper.mapCommentaireToCommentaireDto(this.commentaireRepository.save(
 				commentaireMapper.mapCommentaireDtoToCommentaire(commentaire)));
 	}
--- src/main/java/fr/wijin/fiche/fiche/service/FicheService.java
+++ src/main/java/fr/wijin/fiche/fiche/service/FicheService.java
@@ -1,27 +1,39 @@
 package fr.wijin.fiche.fiche.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import fr.wijin.fiche.fiche.dto.FicheDto;
 import fr.wijin.fiche.fiche.mapper.FicheMapper;
+import fr.wijin.fiche.fiche.mapper.UserMapper;
 import fr.wijin.fiche.fiche.model.Fiche;
 import fr.wijin.fiche.fiche.repository.FicheRepository;
+import fr.wijin.fiche.fiche.repository.UserRepository;
 import jakarta.transaction.Transactional;
 
 @Transactional
 @Service
 public class FicheService {
-    
-    @Autowired
+
+	@Autowired
 	private FicheRepository ficheRepository;
-	
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Autowired
 	private FicheMapper ficheMapper;
-	
+
+	@Autowired
+	private UserMapper userMapper;
+
 	public List<FicheDto> findAll() {
 		return ficheRepository
 				.findAll()
@@ -32,9 +44,9 @@ public class FicheService {
 
 	public Optional<FicheDto> findById(Integer id) {
 		Fiche fiche = ficheRepository.findById(id)
-					.orElse(null);
+				.orElse(null);
 		if (fiche == null) {
-			return Optional.ofNullable( (FicheDto) null);
+			return Optional.ofNullable((FicheDto) null);
 		}
 		return Optional.of(
 				ficheMapper.mapFicheToFicheDto(fiche));
@@ -45,6 +57,12 @@ public class FicheService {
 	}
 
 	public FicheDto save(FicheDto fiche) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			fiche.setAuteurFiche(userMapper.mapUserToUserDto(userRepository.findByUsername(authentication.getName())));
+		}
+		fiche.setDate(LocalDateTime.now());
+		
 		return ficheMapper.mapFicheToFicheDto(this.ficheRepository.save(
 				ficheMapper.mapFicheDtoToFiche(fiche)));
 	}
