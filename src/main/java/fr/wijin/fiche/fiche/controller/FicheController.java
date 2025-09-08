6a2460531470f5159efe14e0d29a97660284d8bd
HeleneHinde <helene.herve@outlook.com>
2025-09-08 15:56:07 +0200

    Fix FicheController mapping for list and show fiche endpoints

--- src/main/java/fr/wijin/fiche/fiche/controller/FicheController.java
+++ src/main/java/fr/wijin/fiche/fiche/controller/FicheController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.wijin.fiche.fiche.dto.FicheDto;
 import fr.wijin.fiche.fiche.service.FicheService;
@@ -16,12 +17,22 @@ public class FicheController {
     @Autowired
     private FicheService ficheService;
 
-    @GetMapping("listfiches")
+    @GetMapping("/listfiches")
     public String showListFiches(Model model){
         List<FicheDto> ficheDtos = ficheService.findAll();
 
         model.addAttribute("fiches", ficheDtos);
 
         return "fiche/listfiches";
+    }
+
+
+    @GetMapping("/showfiche/{id}")
+    public String showficheById(@PathVariable("id") Integer id, Model model) {
+
+        FicheDto ficheDto = ficheService.findById(id).orElse(null);
+
+        model.addAttribute("fiche", ficheDto);
+        return "fiche/showfiche";
     }
 }
