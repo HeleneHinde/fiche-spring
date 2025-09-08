631db39fc789caad5b30d7b78805de6e267bf65b
HeleneHinde <helene.herve@outlook.com>
2025-09-08 09:11:14 +0200

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/fr/wijin/fiche/fiche/FicheApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wijin.fiche.fiche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
