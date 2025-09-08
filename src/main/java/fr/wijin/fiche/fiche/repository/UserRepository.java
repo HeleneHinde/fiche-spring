6c6ad7245f95867f1b78b622565712fe2a597c2f
HeleneHinde <helene.herve@outlook.com>
2025-09-08 10:31:23 +0200

    Add @Repository annotation to CategorieRepository, CommentaireRepository, FicheRepository, and UserRepository interfaces

--- src/main/java/fr/wijin/fiche/fiche/repository/UserRepository.java
+++ src/main/java/fr/wijin/fiche/fiche/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package fr.wijin.fiche.fiche.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.wijin.fiche.fiche.model.Fiche;
 
+@Repository
 public interface UserRepository extends JpaRepository<Fiche, Integer> {
     
 }
